ba81175c999158a392a834aa3bfdf625a21ac894
Chris Chatelain <chris@digiex.ca>
2024-06-07 17:05:38 -0500

    implement a cleanr pio stepper interface (maybe)

--- src/drivers/Motor/Stepper.cpp
+++ src/drivers/Motor/Stepper.cpp
@@ -1,217 +1,70 @@
 #include "Stepper.hpp"
-#include "config.hpp"
-#include "hardware/gpio.h"
-#include "hardware/timer.h"
-#include <algorithm>
-#include <math.h>
-#include <sys/_stdint.h>
-#include <vector>
-
 #include "stepper.pio.h"
+#include <cmath>
+#include <stdio.h>
 
-Stepper::Stepper(int aDirPin, int aStepPin, int anEnablePin, uint32_t aDisableDelay)
-	: myDisableDelay(aDisableDelay), myDirection(DirectionState::STOPPED),
-	  myEnableState(EnableState::DISABLED), myMovementState(MovementState::IDLE), myMoveType(MoveType::DISTANCE),
-	  myTargetSteps(0), myCurrentSteps(0), mySpeed(0), myAcceleration(0), myDeceleration(0)
-{
-	myStepperDevice = new StepperDevice();
-	myStepperDevice->DIR_PIN = aDirPin;
-	myStepperDevice->EN_PIN = anEnablePin;
-	myStepperDevice->STEP_PIN = aStepPin;
-
-	privSetupGPIO();
-	privSetupPIO();
+Stepper::Stepper(uint stepPin, uint dirPin, float maxSpeed, float acceleration, PIO pio, uint sm)
+    : stepPin(stepPin), dirPin(dirPin), maxSpeed(maxSpeed), acceleration(acceleration), pio(pio), sm(sm) {
+    gpio_init(dirPin);
+    gpio_set_dir(dirPin, GPIO_OUT);
 }
 
-void Stepper::SetMaxSpeed(uint32_t aMaxSpeed)
-{
-	// myStepperConfig.maxSpeed = aMaxSpeed;
+void Stepper::InitPIO() {
+    printf("Initializing PIO for stepper...\n");
+    uint offset = pio_add_program(pio, &stepper_program);
+    pio_sm_config c = stepper_program_get_default_config(offset);
+    sm_config_set_sideset_pins(&c, stepPin);  // Configure the step pin for side-set operations
+    sm_config_set_set_pins(&c, stepPin, 1);   // Configure the step pin for set operations
+    pio_gpio_init(pio, stepPin);              // Initialize the GPIO pin
+    pio_sm_set_consecutive_pindirs(pio, sm, stepPin, 1, true); // Set the GPIO direction to output
+    pio_sm_init(pio, sm, offset, &c);         // Initialize the state machine
+    pio_sm_set_enabled(pio, sm, true);        // Enable the state machine
+    printf("PIO initialized.\n");
 }
 
-void Stepper::SetAcceleration(uint32_t aAcceleration)
-{
-	// myAcceleration = aAcceleration;
+void Stepper::SetDirection(bool direction) {
+    this->direction = direction;
+    gpio_put(dirPin, direction);
+    printf("Direction set to %d\n", direction);
 }
 
-void Stepper::SetDeceleration(uint32_t aDeceleration)
-{
-	// myDeceleration = aDeceleration;
+void Stepper::GenerateDelays(std::vector<uint32_t> &delays) {
+    printf("Generating delays...\n");
+    float accelSteps = pow(maxSpeed, 2) / (2 * acceleration);
+    int cruiseSteps = totalSteps - 2 * accelSteps;
+
+    for (int step = 0; step < accelSteps; ++step) {
+        float stepDelay = 1e6 / (sqrt(2 * acceleration * step));
+        if (stepDelay > 0xFFFF) stepDelay = 0xFFFF; // Bound the delay value
+        delays.push_back((uint32_t)stepDelay);
+    }
+
+    float maxStepDelay = 1e6 / maxSpeed;
+    if (maxStepDelay > 0xFFFF) maxStepDelay = 0xFFFF; // Bound the delay value
+    for (int step = 0; step < cruiseSteps; ++step) {
+        delays.push_back((uint32_t)maxStepDelay);
+    }
+
+    for (int step = accelSteps; step > 0; --step) {
+        float stepDelay = 1e6 / (sqrt(2 * acceleration * step));
+        if (stepDelay > 0xFFFF) stepDelay = 0xFFFF; // Bound the delay value
+        delays.push_back((uint32_t)stepDelay);
+    }
+    printf("Delays generated.\n");
 }
 
-void Stepper::SetDisableDelay(uint32_t aDisableDelay)
-{
-	// myDisableDelay = aDisableDelay;
-}
+void Stepper::Move(int totalSteps) {
+    this->totalSteps = totalSteps;
+    std::vector<uint32_t> delays;
+    GenerateDelays(delays);
 
-void Stepper::Enable()
-{
-	gpio_put(myStepperDevice->EN_PIN, STEPPER1_ENABLE_POLARITY);
-}
-
-void Stepper::Disable()
-{
-	gpio_put(myStepperDevice->EN_PIN, !STEPPER1_ENABLE_POLARITY);
-}
-
-void Stepper::Move(int aSteps, int aSpeed)
-{
-	// // todo: cancel disable timer
-	// if (myEnableState == EnableState::DISABLED)
-	// {
-	// 	Enable();
-	// }
-
-	// DirectionState direction = (aSteps > 0) ? DirectionState::POS : DirectionState::NEG;
-	// gpio_pin_set_dt(&myStepperConfig.dirGpio, (direction == DirectionState::POS) ? myStepperConfig.dirPolarity : !myStepperConfig.dirPolarity);
-
-	// std::vector<int> delays;
-	// CalculateSpeedProfile(aSteps, aSpeed, delays);
-
-	// for (int delay : delays)
-	// {
-	// 	StepCommand *command = new StepCommand{delay};
-	// 	k_fifo_put(&myMovementQueue, command);
-	// }
-
-	// UpdateMovement();
-}
-
-void Stepper::Stop()
-{
-	// // Flush the current movement queue
-	// StepCommand *command;
-	// while ((command = (StepCommand *)k_fifo_get(&myMovementQueue, K_NO_WAIT)) != NULL)
-	// {
-	// 	delete command;
-	// }
-
-	// // Add deceleration steps to stop the motor
-
-	// std::vector<int> delays;
-	// int targetSpeed = 0;
-	// int stepsToStop = (mySpeed * mySpeed) / (2 * myDeceleration);
-	// CalculateSpeedProfile(stepsToStop, targetSpeed, delays);
-
-	// for (int delay : delays)
-	// {
-	// 	StepCommand *command = new StepCommand{delay};
-	// 	k_fifo_put(&myMovementQueue, command);
-	// }
-
-	// UpdateMovement();
-}
-
-void Stepper::MoveRelative(int steps, DirectionState dir)
-{
-	privMoveRelative(steps, dir);
-}
-
-bool Stepper::Update()
-{
-	return privUpdatePendingMovement();
-}
-
-uint32_t Stepper::GetRemainingSteps()
-{
-	return myStepperDevice->StepsToTake;
-}
-
-// todo: change this to just deny changing direction if there steps in the fifo. maybe I do need this for async, but then it's more of... decelerate to a stop then reverse and move to the intended position. Maybe make this position tracking rather than relative step execution?
-bool Stepper::privChangeMotorDirection()
-{
-	if (myStepperDevice->activedir != myStepperDevice->dirchange && myStepperDevice->directionChangeDelayCounter == 0) // first change detected
-	{
-		if (pio_sm_is_tx_fifo_empty(myStepperDevice->stm_pio, myStepperDevice->stm_sm))
-		{
-			myStepperDevice->directionChangeDelayCounter = (time_us_64() / 1000);
-		}
-		return false;
-	}
-	else if (myStepperDevice->activedir != myStepperDevice->dirchange && myStepperDevice->directionChangeDelayCounter != 0)
-	{
-		if (((time_us_64() / 1000) - myStepperDevice->directionChangeDelayCounter) > myStepperDevice->directionChangeDelay) // pre change delay
-		{
-			myStepperDevice->activedir = myStepperDevice->dirchange;
-			gpio_put(myStepperDevice->DIR_PIN, !myStepperDevice->activedir);
-			myStepperDevice->directionChangeDelayCounter = (time_us_64() / 1000);
-		}
-		return false;
-	}
-	else if (myStepperDevice->activedir == myStepperDevice->dirchange && myStepperDevice->directionChangeDelayCounter != 0)
-	{
-		if (((time_us_64() / 1000) - myStepperDevice->directionChangeDelayCounter) > myStepperDevice->directionChangeDelay) // post change delay
-		{
-			myStepperDevice->directionChangeDelayCounter = 0;
-			return true;
-		}
-		return false;
-	}
-
-	return true;
-}
-
-void Stepper::privSetupPIO()
-{
-	// stepper 1 pio
-	uint stm_offset = pio_add_program(myStepperDevice->stm_pio, &stepper_1_program);
-	stepper_1_program_init(myStepperDevice->stm_pio, myStepperDevice->stm_sm, stm_offset, myStepperDevice->STEP_PIN, 10000, true);
-}
-
-void Stepper::privSetupGPIO()
-{
-
-	// stepper enable pin
-	gpio_init(myStepperDevice->EN_PIN);
-	Disable();
-	// gpio_put(STM_EN, 0);
-
-	// wheel 1 stepper dir pin
-	gpio_init(myStepperDevice->DIR_PIN);
-	gpio_set_dir(myStepperDevice->DIR_PIN, GPIO_OUT);
-	// gpio_put(myStepperDevice->DIR_PIN, 1);
-}
-
-bool Stepper::privUpdatePendingMovement()
-{
-
-	if (myStepperDevice->StepsToTake > 0 && pio_sm_is_tx_fifo_empty(myStepperDevice->stm_pio, myStepperDevice->stm_sm))
-	{
-		if (privChangeMotorDirection())
-		{
-			int remBits = 32;
-			if (myStepperDevice->StepsToTake < 32)
-			{
-				remBits = myStepperDevice->StepsToTake;
-			}
-
-			privPutStepsBlocking(remBits);
-			myStepperDevice->StepsToTake -= remBits;
-		}
-	}
-
-	return (myStepperDevice->StepsToTake > 0);
-}
-
-void Stepper::privMoveRelative(int steps, DirectionState dir)
-{
-	myStepperDevice->StepsToTake += steps * static_cast<int>(dir);
-}
-
-void Stepper::privPutStepsBlocking(uint32_t steps)
-{
-	uint32_t aPulseCount;
-	if (steps <= 0)
-	{
-		aPulseCount = 0;
-	}
-	else if (steps >= 32)
-	{
-		aPulseCount = 0xFFFFFFFF; // 32 bits of '1'
-	}
-	else
-	{
-		aPulseCount = (1U << steps) - 1;
-	}
-
-	pio_sm_put_blocking(pio0, 1, aPulseCount);
+    for (auto delay : delays) {
+        printf("Pushing delay: %u\n", delay);
+        while (pio_sm_is_tx_fifo_full(pio, sm)) {
+            // Wait until there is space in the FIFO
+            tight_loop_contents();
+        }
+        pio_sm_put_blocking(pio, sm, delay);
+    }
+    printf("Move complete.\n");
 }
--- src/drivers/Motor/Stepper.hpp
+++ src/drivers/Motor/Stepper.hpp
@@ -1,166 +1,28 @@
-#pragma once
-#include "Enum.hpp"
+#ifndef STEPPER_HPP
+#define STEPPER_HPP
+
+#include <vector>
+#include "pico/stdlib.h"
 #include "hardware/pio.h"
-#include <cstdint>
 
-struct StepperDevice
-{
-    int StepsToTake = 0;
-    bool activedir = true;
-    bool dirchange = true;
-    float ActiveAngle = 0.0; // 360/res
-    int DIR_PIN = 11;
-    int STEP_PIN = 10;
-	int EN_PIN = 9;
-    int directionChangeDelayCounter = 0;
-    PIO stm_pio = pio0;
-    int stm_sm;
-	int directionChangeDelay = 50;
-};
-
-/**
- * @brief Stepper motor driver
- *
- * This class provides an interface to control a stepper motor.
- * @param aStepperConfig The configuration for the stepper motor.
- * @param aMaxSpeed The maximum speed for the stepper motor in steps per second.
- * @param aMaxAcceleration The maximum acceleration for the stepper motor in steps per second^2.
- * @param aMaxDeceleration The maximum deceleration for the stepper motor in steps per second^2.
- * @param aDisableDelay The delay in milliseconds before disabling the stepper motor after stopping. 0 means never disable.
- */
-
-class Stepper
-{
-
+class Stepper {
 public:
-	enum class DirectionState
-	{
-		POS = 1,
-		NEG = -1,
-		STOPPED = 0
-	};
-	enum class EnableState
-	{
-		ENABLED = 1,
-		DISABLED = 0
-	};
-	enum class MovementState
-	{
-		IDLE,
-		ACCELERATING,
-		DECELERATING,
-		CONSTANT_SPEED
-	};
-
-	enum class MoveType
-	{
-		DISTANCE,
-		CONTINUOUS
-	};
-
-	Stepper() = delete;
-
-	Stepper(int aDirPin, int aStepPin, int anEnablePin, uint32_t aDisableDelay = 0);
-
-	/**
-	 * @brief Set the maximum speed for the stepper motor.
-	 *
-	 * @param aMaxSpeed The maximum speed for the stepper motor in steps per second.
-	 */
-	void SetMaxSpeed(uint32_t aMaxSpeed);
-
-	/**
-	 * @brief Set the acceleration for the stepper motor.
-	 *
-	 * @param aMaxAcceleration The acceleration for the stepper motor in steps per second^2.
-	 */
-	void SetAcceleration(uint32_t aAcceleration);
-
-	/**
-	 * @brief Set the  deceleration for the stepper motor.
-	 *
-	 * @param aMaxDeceleration The deceleration for the stepper motor in steps per second^2. 0 means it will use the acceleration value.
-	 */
-	void SetDeceleration(uint32_t aDeceleration);
-
-	/**
-	 * @brief Set the delay before disabling the stepper motor after stopping.
-	 *
-	 * @param aDisableDelay The delay in milliseconds before disabling the stepper motor after stopping. 0 means never disable.
-	 */
-	void SetDisableDelay(uint32_t aDisableDelay);
-
-	/**
-	 * @brief Enable the stepper motor.
-	 */
-	void Enable();
-
-	/**
-	 * @brief Disable the stepper motor.
-	 * If the motor is moving, this will flush the movement queue and stop without decellerating.
-	 */
-	void Disable();
-
-	/**
-		* @brief Move the stepper motor a number of steps at a given speed.
-		* If moves are in progress, it will add on steps to the current movement queue.
-		*
-		* @param aSteps The number of steps to move. Positive values move the motor in the positive direction, negative values move the motor in the negative direction.
-		* @param aSpeed The speed to move the motor in steps per second.
-
-	*/
-	void Move(int steps, int speed);
-
-	void MoveRelative(int steps, DirectionState dir);
-
-	/**
-		* @brief Move the stepper motor continuously in a given direction at a given speed.
-		* This continually adds a movement to the movement queue at the same speed as the moves are removed from the movement queue and executed, while respecting acceleration and deceleration.
-		* If it is already moving, it will change the speed. If it is already moving and a different direction is requested, it will stop and start moving in the new direction.
-		*
-		* @param aDirection The direction to move the motor.
-		* @param aSpeed The speed to move the motor in steps per second.
-
-	*/
-	void MoveContinuous(DirectionState aDirection, int aSpeed);
-
-	/**
-	 * @brief Stop the stepper motor.
-	 * It will flush the current movement queue and start decelerating from the current speed.
-	 */
-	void Stop();
-
-	/**
-	 * @brief Update the stepper motor.
-	 * This should be called periodically to update the stepper motor state in case
-	 * the initial move did not completely finish all of the steps.
-	 * @return true if the stepper motor is still moving, false if it has stopped.
-	 */
-	bool Update();
-
-	uint32_t GetRemainingSteps();
+    Stepper(uint stepPin, uint dirPin, float maxSpeed, float acceleration, PIO pio, uint sm);
+    void InitPIO();
+    void SetDirection(bool direction);
+    void Move(int totalSteps);
 
 private:
-	StepperDevice* myStepperDevice;
+    void GenerateDelays(std::vector<uint32_t> &delays);
 
-	uint32_t myDisableDelay;
-
-	DirectionState myDirection = DirectionState::STOPPED;
-	EnableState myEnableState = EnableState::DISABLED;
-	MovementState myMovementState = MovementState::IDLE;
-	MoveType myMoveType = MoveType::DISTANCE;
-	int myTargetSteps = 0;
-	int myCurrentSteps = 0;
-	int mySpeed = 0;
-	int myAcceleration = 0;
-	int myDeceleration = 0;
-
-	bool privChangeMotorDirection();
-	void privSetupPIO();
-	void privSetupGPIO();
-	inline uint32_t privConstruct32bits(int pulsecnt);
-	bool privUpdatePendingMovement();
-	float privCountAngle(float currentAngle, float counterVal, bool dir); //???
-	void privPutStepsBlocking(uint32_t steps);
-	void privMoveRelative(int steps, DirectionState dir); //(degree position, steppernumber, direction to move (0-fastest route,1-forward,2-backward))
+    uint stepPin;
+    uint dirPin;
+    bool direction;
+    float maxSpeed; // steps per second
+    float acceleration; // steps per second squared
+    int totalSteps;
+    PIO pio;
+    uint sm;
 };
+
+#endif // STEPPER_HPP
